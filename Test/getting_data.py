a6f9fc54a269484fbbdd0c1bc00850b0a1c5d144
lbetancourthc <82390922+lbetancourthc@users.noreply.github.com>
2022-02-15 12:25:16 -0500

    Update getting_data.py

--- Test/getting_data.py
+++ Test/getting_data.py
@@ -1,1 +1,9 @@
+# Getting and displaying data from REST Countries
+import requests
 
+url = 'https://restcountries.com/v3.1/all'  # This is a link of rest country API
+data_url = requests.get(url)                # Request data to URL
+data = data_url.json()                      # JSON data for display
+
+print(data[0])                              # Displaying data content
+print(data[0].keys())                       # Duisplaying keys for data content
